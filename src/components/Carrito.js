5d189b2a08df6b6361423a3405950ede97d7fcd0
jpsellanes <sellanes_juan_pablo@hotmail.com>
2022-04-02 06:48:29 -0300

    DesafioClase13

--- src/components/Carrito.js
+++ src/components/Carrito.js
@@ -1,12 +1,34 @@
 import React,{useContext} from "react";
 import { CartContext } from "../context/CartContext";
-
-
+import { addDoc, collection , serverTimestamp  } from "firebase/firestore";
+import {dbFirebase} from "../firebaseConfig"
 
 const Carrito = ()=>{
 
     const {addItem, removeItem, clear, isInCart, cart, cartTotal}= useContext(CartContext)
+
+    const confirmCompra = () =>{
+        const orden ={
+            buyer : {
+                name : "",
+                phone : "",
+                email : ""
+            },
+            items : cart,
+            date : serverTimestamp(),
+            total : cartTotal(cart)
+        }
+
+        const ordenCollection = collection(dbFirebase, "ordenes")
+        const pedido = addDoc(ordenCollection, orden)
+
+        pedido.then(res =>{
+            console.log(res.id)
+        })
+    }
+
     
+
     return(
         <div>
             <button onClick={clear}>Eliminar Carrito</button>
@@ -25,6 +47,7 @@ const Carrito = ()=>{
             ))}
             <div>
                 <h3>Cart Total = U$D {cartTotal(cart)}</h3>
+                <button onClick={confirmCompra}>Confirmar Compra</button>
             </div>
         </div>
         
