e3c2a4fa6a84f7dd018b362829a8171831d79c44
jpsellanes <sellanes_juan_pablo@hotmail.com>
2022-04-03 11:35:35 -0300

    DesafioClase13v3

--- src/components/Carrito.js
+++ src/components/Carrito.js
@@ -2,7 +2,9 @@ import React,{useContext} from "react";
 import { CartContext } from "../context/CartContext";
 import { addDoc, collection , serverTimestamp  } from "firebase/firestore";
 import {dbFirebase} from "../firebaseConfig"
+import {Button} from "@mui/material"
 import CartForm from "./CartForm"
+import "./carrito.css"
 
 const Carrito = ()=>{
 
@@ -11,28 +13,29 @@ const Carrito = ()=>{
     
 
     return(
-        <div>
-            {cart.map(item=>(
-                <div key={item.product.id}>
-                    <div>
-                        <p>{item.product.nombre}</p>
-                        <img src={item.product.imgurl} />
-                        <p>Product ID={item.product.id}</p>
-                        <p>Precio U$D {item.product.precio}</p>
-                        <p>Amount {item.count}</p>
-                        <p>Sub-Total U$D {item.count * item.product.precio}</p>
-                        <button onClick={()=>removeItem(item.product)}>Remove from Cart</button>
+        <div className="carrito">
+            <div className="carritoComponent">
+                {cart.map(item=>(
+                    <div key={item.product.id}>
+                        <div className="carritoProducto">
+                            <p>{item.product.nombre}</p>
+                            <img src={item.product.imgurl} />
+                            <p>Product ID={item.product.id}</p>
+                            <p>Precio U$D {item.product.precio}</p>
+                            <p>Amount {item.count}</p>
+                            <p>Sub-Total U$D {item.count * item.product.precio}</p>
+                            <Button style ={{backgroundColor: "Red"}} variant="contained" size="small" onClick={()=>removeItem(item.product)}>Remove from Cart</Button>
+                        </div>
                     </div>
-                </div>
-            ))}
-            <div>
-                <h3>Cart Total = U$D {cartTotal(cart)}</h3>
-                <button onClick={clear}>Eliminar Carrito</button>
+                ))}
             </div>
-            <div>
+            <div className="carritoComponent">
+                <h3>Cart Total = U$D {cartTotal(cart)}</h3>
+                <Button style ={{backgroundColor: "Red"}} variant="contained" onClick={clear}>Clear Cart</Button>
                 <CartForm/>
             </div>
         </div>
+        
         
     )
 }
